d28a1ccf31fc8d841b7b1e869aa0d58281a65776
Nikos <nikos.sklikas@canonical.com>
2024-04-29 11:09:30 +0300

    fix: use correct grant lifespan to issue tokens

--- handler/rfc8628/token_handler.go
+++ handler/rfc8628/token_handler.go
@@ -109,6 +109,10 @@ func (v DeviceAccessRequestValidator) ValidateRedirectURI(accessRequester fosite
 	return nil
 }
 
+func (v DeviceAccessRequestValidator) GetGrantType(requester fosite.AccessRequester) fosite.GrantType {
+	return fosite.GrantTypeDeviceCode
+}
+
 type DeviceCodeTokenEndpointHandler struct {
 	oauth2.GenericCodeTokenEndpointHandler
 }
